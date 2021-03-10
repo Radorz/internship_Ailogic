8a0685b3ff3d31ab2a92a0e8e7d2025cd3e80afb
Radorz <Radorz@users.github.com>
2021-03-10 11:38:14 -0400

    Uncomment repos

---
+++ internship_Ailogic/Controllers/RequestController.cs
@@ -0,0 +1,50 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internship_Ailogic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+
+
+        private readonly RequestInternshipRepository _requestInternshiprepo;
+
+        public RequestController(RequestInternshipRepository requestInternshiprepo)
+        {
+            _requestInternshiprepo = requestInternshiprepo;
+
+        }
+
+        [HttpPost("apply")]
+        public async Task<ActionResult> Apply(ApplyInternshipDTO userInfo)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var response = await _requestInternshiprepo.Apply(userInfo);
+                if (response)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+    }
+}
