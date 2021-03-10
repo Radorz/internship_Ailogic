e5564f6cfab518bf1249a5b146a97cd7b64d9d4f
David <David0nike@gmail.com>
2021-03-10 17:02:13 -0400

    Class Utilities Added

---
+++ internship_Ailogic/Helpers/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internship_Ailogic.Helpers
+{
+    public class Utilities
+    {
+
+        
+
+
+
+    }
+}
