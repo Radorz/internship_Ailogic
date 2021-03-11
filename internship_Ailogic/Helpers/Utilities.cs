7d8e7041f71552aa458efd9f7aeeb662a9bc5fc7
David <David0nike@gmail.com>
2021-03-11 15:30:46 -0400

    Utilities methods addeds successfully

--- internship_Ailogic/Helpers/Utilities.cs
+++ internship_Ailogic/Helpers/Utilities.cs
@@ -16,18 +16,40 @@ namespace internship_Ailogic.Helpers
 
        public string FormatCedula(string cedula)
        {
-            if (cedula.Length == 13)
+            if (cedula.Contains("-") && cedula.Length == 13)
             {
                 return cedula;
             }
-            else
+             else if(cedula.Length == 11 && !cedula.Contains("-"))
             {
                 cedula.Insert(2, "-");
                 cedula.Insert(11, "-");
                 return cedula;
             }
-       } 
-        
+            else
+            {
+                return "Invalid Cedula";
+            }
+       }
+
+        public string FormatPhone(string phone)
+        {
+            if (phone.Contains("-") && phone.Length == 12)
+            {
+                return phone;
+            }
+            else if (phone.Length == 10 && !phone.Contains("-"))
+            {
+                phone.Insert(3, "-");
+                phone.Insert(7, "-");
+                return phone;
+            }
+            else
+            {
+                return "Invalid Phone";
+            }
+        }
+
 
 
 
