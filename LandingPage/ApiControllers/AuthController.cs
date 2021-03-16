ee8d686dd89bf7a101b8a131a3700d076565d185
Radorz <Radorz@users.github.com>
2021-03-16 12:53:40 -0400

    Migration api project to internship project

---
+++ LandingPage/ApiControllers/AuthController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Database.Models;
+using DTO;
+using Repository.Repository;
+using internship_Ailogic.Helpers;
+
+namespace LandingPage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly Utilities _utilities;
+
+
+        public AuthController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration,
+            RequestInternshipRepository requestInternshipDTO,
+            Utilities utilities)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _utilities = utilities;
+        }
+
+
+        [HttpPost("create")]
+        public async Task<ActionResult<UserToken>> CreateUser([FromBody] UserInfo model)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                List<string> role = new List<string>();
+                role.Add("Intern");
+                return _utilities.BuildToken(model, role);
+            }
+            else
+            {
+                return BadRequest("Username or password invalid");
+            }
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola";
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] UserInfo userInfo)
+
+        {  if (ModelState.IsValid)
+            {
+                var find = await _userManager.FindByEmailAsync(userInfo.Email);
+                if (find != null) {
+                    var result = await _signInManager.PasswordSignInAsync(find.UserName, userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        var user = await _userManager.FindByEmailAsync(userInfo.Email);
+                        var roles = await _userManager.GetRolesAsync(user);
+                        return _utilities.BuildToken(userInfo, roles);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return BadRequest(ModelState);
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return BadRequest(ModelState);
+                }
+
+            }
+            else
+            {
+                return BadRequest(ModelState);
+
+            }
+        }
+
+       
+
+
+
+      
+    }
+
+}
---
+++ LandingPage/ApiControllers/InternshipController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DTO;
+using Repository;
+using Repository.Repository;
+using Database.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace LandingPage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+    public class InternshipController : ControllerBase
+    {
+
+        private readonly InternshipsRepository _internshipsRepository;
+        public InternshipController(InternshipsRepository internshipsRepository)
+        {
+            this._internshipsRepository = internshipsRepository;
+        }
+
+        [HttpGet]
+        public async Task<List<InternshipsDTO>> Get()
+        {
+            return await _internshipsRepository.GetAllCustom();
+        }
+
+        [HttpGet("{id}", Name = "GetInternship")]
+        public async Task<ActionResult<InternshipsDTO>> Get(int id)
+        {
+            var result = await _internshipsRepository.GetByIdCustom(id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Save(InternshipsDTOPost DTO)
+        {
+            if (ModelState.IsValid)
+            {
+               await _internshipsRepository.AddCustom(DTO);
+                return Ok(DTO);
+            }
+            else
+            {
+                return BadRequest();
+            }
+            
+            
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var internship =  await _internshipsRepository.Delete(id);
+            if (internship != null)
+            {
+                return Ok("Se ha borrado correctamente");
+            }
+            else
+            {
+                return NotFound("No se ha podido borrar");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<InternshipsDTO>> Update(int id, InternshipsDTOPost dto)
+        {
+
+            await _internshipsRepository.UpdateCustom(id, dto);
+            if (ModelState.IsValid)
+            {
+                return StatusCode(201);
+            }
+            else
+            {
+                return BadRequest("No se ha podido actualizar");
+            }
+
+
+        }
+      
+
+    }
+}
