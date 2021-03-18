140093dab9b543218e6fff713d16be540de50c46
Radorz <Radorz@users.github.com>
2021-03-18 12:03:39 -0400

    debug Fileednpoint to send imgs

--- LandingPage/ApiControllers/FilesController.cs
+++ LandingPage/ApiControllers/FilesController.cs
@@ -48,7 +48,7 @@ namespace LandingPage.ApiControllers
             {
                 var file = Request.Form.Files[0];
                 var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                var pathToSave = Path.Combine("https://ailogicinternship.azurewebsites.net", folderName);
                 if (file.Length > 0)
                 {
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName;
