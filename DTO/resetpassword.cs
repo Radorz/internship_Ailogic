9b0159a3728b67cfbb28cad57a05638b9aa6b7f0
Radorz <Radorz@users.github.com>
2021-03-23 15:05:58 -0400

    validation for dto resetpasword

--- DTO/resetpassword.cs
+++ DTO/resetpassword.cs
@@ -13,7 +13,10 @@ namespace DTO
 
 
         [Required(ErrorMessage = "The password  is required")]
-        public string Password { get; set; }    
+        public string Password { get; set; }
 
+        [Required(ErrorMessage = "The confirmpassword  is required")]
+        [Compare("Password", ErrorMessage = "The passwords fields do not match.")]
+        public string confirmpassword { get; set; }
     }
 }
